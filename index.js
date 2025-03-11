1e0a3923654c5cbf598cdb3559fce5fd03f98839
Sawer <danilch4024@gmail.com>
2025-03-11 16:39:09 +0500

    add.. play

--- index.js
+++ index.js
@@ -4,24 +4,101 @@ function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            const action = animaster()
+                .addFadeIn(100)
+                .addMove(200, {x: 40, y: 40})
+                .addScale(800, 1.3)
+                .addMove(200, {x: 80, y: 0})
+                .addScale(800, 1)
+                .addMove(200, {x: 40, y: -40})
+                .addScale(800, 0.7)
+                .addMove(200, {x: 0, y: 0})
+                .addScale(800, 1)
+                .play(block);
+            setTimeout(() => {
+                action.reset();
+            }, 1000);
+        });
+
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
+            const action = animaster().fadeOut(block, 10000);
+            setTimeout(() => {
+                action.reset();
+            }, 3000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            animaster().move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            animaster().scale(block, 1000, 1.25);
         });
 }
 
 function animaster() {
     return {
+        _steps: [],
+
+        _addStep(duration, stepFunc) {
+            this._steps.push([duration, stepFunc]);
+        },
+
+        _playStep(element, stepId, state) {
+            if (stepId >= this._steps.length) {
+                return;
+            }
+            const [duration, func] = this._steps[stepId];
+            state.history.push(func(element, duration));
+            state.currentTimeout = setTimeout(() => {
+                this._playStep(element, stepId + 1, state);
+            }, duration);
+        },
+
+        play(element) {
+            const state = {
+                history: []
+            };
+            this._playStep(element, 0, state);
+            return {
+                stop() {
+                    clearTimeout(state.currentTimeout);
+                    state.history[-1].stop?.();
+                },
+                reset() {
+                    clearTimeout(state.currentTimeout);
+                    for (const step of state.history.reverse())
+                        step.reset?.();
+                }
+            };
+        },
+
+        addFadeIn(duration) {
+            this._addStep(duration, this.fadeIn);
+            return this;
+        },
+
+        addFadeOut(duration) {
+            this._addStep(duration, this.fadeOut);
+            return this;
+        },
+
+        addMove(duration, translation) {
+            this._addStep(duration, (element, duration) => this.move(element, duration, translation));
+            return this;
+        },
+
+        addScale(duration, ratio) {
+            this._addStep(duration, (element, duration) => this.scale(element, duration, ratio));
+            return this;
+        },
+
         /**
          * Блок плавно появляется из прозрачного.
          * @param element — HTMLElement, который надо анимировать
@@ -31,6 +108,13 @@ function animaster() {
             element.style.transitionDuration = `${duration}ms`;
             element.classList.remove('hide');
             element.classList.add('show');
+            return {
+                reset() {
+                    element.style.transitionDuration = null;
+                    element.classList.remove('show');
+                    element.classList.add('hide');
+                }
+            };
         },
 
         /**
@@ -42,6 +126,13 @@ function animaster() {
             element.style.transitionDuration = `${duration}ms`;
             element.classList.remove('show');
             element.classList.add('hide');
+            return {
+                reset() {
+                    element.style.transitionDuration = null;
+                    element.classList.remove('hide');
+                    element.classList.add('show');
+                }
+            };
         },
 
         /**
@@ -53,6 +144,12 @@ function animaster() {
         move(element, duration, translation) {
             element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(translation, null);
+            return {
+                reset() {
+                    element.style.transitionDuration = null;
+                    element.style.transform = getTransform({ x: 0, y: 0 }, null);
+                }
+            };
         },
 
         /**
@@ -62,12 +159,16 @@ function animaster() {
          * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
          */
         scale(element, duration, ratio) {
-            element.style.transitionDuration =  `${duration}ms`;
+            element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(null, ratio);
+            return {
+                stop() {
+                    element.style.transitionDuration = null;
+                    element.style.transform = getTransform(null, 1);
+                }
+            };
         }
     }
-}
-
 }
 
 function getTransform(translation, ratio) {
